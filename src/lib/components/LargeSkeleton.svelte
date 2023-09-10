99e329e1750a3aabccbb66e9bbbbfac8cfdfb4bd
H3nry-T <ht3drex@gmail.com>
2023-09-10 16:26:40 +0100

    skeleton is more responsive

--- src/lib/components/LargeSkeleton.svelte
+++ src/lib/components/LargeSkeleton.svelte
@@ -5,14 +5,14 @@
 <section class="flex flex-col items-center min-w-full min-h-screen gap-20 py-20 bg-background">
 	<Skeleton class="w-[90%] h-[100px] rounded-lg" />
 
-	<section class="grid w-full max-w-lg grid-cols-1 md:grid-cols-3 md:max-w-7xl gap-96">
+	<section class="grid w-full max-w-md grid-cols-1 gap-4 md:grid-cols-3 md:max-w-7xl">
 		<section class="flex flex-col self-center gap-20 justify-self-center">
 			{#each new Array(5).fill(0) as _, i (i)}
 				<div class="flex items-center gap-4">
 					<Skeleton class="w-12 h-12 rounded-full" />
 					<div class="space-y-2">
-						<Skeleton class="h-4 w-[250px]" />
-						<Skeleton class="h-4 w-[200px]" />
+						<Skeleton class="h-4 sm:w-[150px] md:w-[170px] w-[200px]" />
+						<Skeleton class="h-4 sm:w-[150px] md:w-[170px] w-[200px]" />
 					</div>
 				</div>
 			{/each}
@@ -22,8 +22,8 @@
 				<div class="flex items-center gap-4">
 					<Skeleton class="w-12 h-12 rounded-full" />
 					<div class="space-y-2">
-						<Skeleton class="h-4 w-[250px]" />
-						<Skeleton class="h-4 w-[200px]" />
+						<Skeleton class="h-4 sm:w-[150px] md:w-[170px] w-[200px]" />
+						<Skeleton class="h-4 sm:w-[150px] md:w-[170px] w-[200px]" />
 					</div>
 				</div>
 			{/each}
@@ -33,8 +33,8 @@
 				<div class="flex items-center gap-4">
 					<Skeleton class="w-12 h-12 rounded-full" />
 					<div class="space-y-2">
-						<Skeleton class="h-4 w-[250px]" />
-						<Skeleton class="h-4 w-[200px]" />
+						<Skeleton class="h-4 sm:w-[150px] md:w-[170px] w-[200px]" />
+						<Skeleton class="h-4 sm:w-[150px] md:w-[170px] w-[200px]" />
 					</div>
 				</div>
 			{/each}
