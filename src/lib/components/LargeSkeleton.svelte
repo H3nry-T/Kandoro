eede69a4b45f22210b6f9547e9ab7b42e6570aeb
H3nry-T <ht3drex@gmail.com>
2023-09-10 15:57:25 +0100

    large skeleton created

---
+++ src/lib/components/LargeSkeleton.svelte
@@ -0,0 +1,43 @@
+<script>
+	import Skeleton from './ui/skeleton/skeleton.svelte';
+</script>
+
+<section class="flex flex-col items-center min-w-full min-h-screen gap-20 py-20 bg-background">
+	<Skeleton class="w-[90%] h-[100px] rounded-lg" />
+
+	<section class="grid w-full max-w-lg grid-cols-1 md:grid-cols-3 md:max-w-7xl gap-96">
+		<section class="flex flex-col self-center gap-20 justify-self-center">
+			{#each new Array(5).fill(0) as _, i (i)}
+				<div class="flex items-center gap-4">
+					<Skeleton class="w-12 h-12 rounded-full" />
+					<div class="space-y-2">
+						<Skeleton class="h-4 w-[250px]" />
+						<Skeleton class="h-4 w-[200px]" />
+					</div>
+				</div>
+			{/each}
+		</section>
+		<section class="flex-col self-center hidden gap-20 justify-self-center md:flex">
+			{#each new Array(5).fill(0) as _, i (i)}
+				<div class="flex items-center gap-4">
+					<Skeleton class="w-12 h-12 rounded-full" />
+					<div class="space-y-2">
+						<Skeleton class="h-4 w-[250px]" />
+						<Skeleton class="h-4 w-[200px]" />
+					</div>
+				</div>
+			{/each}
+		</section>
+		<section class="flex-col self-center hidden gap-20 justify-self-center md:flex">
+			{#each new Array(5).fill(0) as _, i (i)}
+				<div class="flex items-center gap-4">
+					<Skeleton class="w-12 h-12 rounded-full" />
+					<div class="space-y-2">
+						<Skeleton class="h-4 w-[250px]" />
+						<Skeleton class="h-4 w-[200px]" />
+					</div>
+				</div>
+			{/each}
+		</section>
+	</section>
+</section>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import ColorPalette from '$lib/components/ColorPalette.svelte';
 	import KanbanColumn from '$lib/components/KanbanPage/KanbanColumn.svelte';
+	import LargeSkeleton from '$lib/components/LargeSkeleton.svelte';
 	import ToggleLightDarkMode from '$lib/components/ToggleLightDarkMode.svelte';
 	import CardTransition from '$lib/components/animation/CardTransition.svelte';
 	import Button from '$lib/components/ui/button/button.svelte';
@@ -48,59 +49,21 @@
 </script>
 
 <section>
-	{#if !loading}
+	{#if loading}
+		<LargeSkeleton />
+	{:else}
 		<nav class="flex items-center justify-between w-full gap-4 px-4 py-5 md:p-6">
 			<ToggleLightDarkMode />
 			{#if $userStore}
 				<Button variant="default" on:click={handleSignOut}><LogOut size={22} /></Button>
 			{/if}
 		</nav>
-		{#if !$userStore}
-			<Auth />
-		{:else}
+		{#if $userStore}
 			<CardTransition>
 				<slot />
 			</CardTransition>
+		{:else}
+			<Auth />
 		{/if}
-	{:else}
-		<section class="flex flex-col items-center min-w-full min-h-screen gap-20 py-20 bg-background">
-			<Skeleton class="w-[90%] h-[100px] rounded-lg" />
-
-			<section class="grid w-full max-w-lg grid-cols-1 md:grid-cols-3 md:max-w-7xl gap-96">
-				<section class="flex flex-col self-center gap-20 justify-self-center">
-					{#each new Array(5).fill(0) as _, i (i)}
-						<div class="flex items-center gap-4">
-							<Skeleton class="w-12 h-12 rounded-full" />
-							<div class="space-y-2">
-								<Skeleton class="h-4 w-[250px]" />
-								<Skeleton class="h-4 w-[200px]" />
-							</div>
-						</div>
-					{/each}
-				</section>
-				<section class="flex-col self-center hidden gap-20 justify-self-center md:flex">
-					{#each new Array(5).fill(0) as _, i (i)}
-						<div class="flex items-center gap-4">
-							<Skeleton class="w-12 h-12 rounded-full" />
-							<div class="space-y-2">
-								<Skeleton class="h-4 w-[250px]" />
-								<Skeleton class="h-4 w-[200px]" />
-							</div>
-						</div>
-					{/each}
-				</section>
-				<section class="flex-col self-center hidden gap-20 justify-self-center md:flex">
-					{#each new Array(5).fill(0) as _, i (i)}
-						<div class="flex items-center gap-4">
-							<Skeleton class="w-12 h-12 rounded-full" />
-							<div class="space-y-2">
-								<Skeleton class="h-4 w-[250px]" />
-								<Skeleton class="h-4 w-[200px]" />
-							</div>
-						</div>
-					{/each}
-				</section>
-			</section>
-		</section>
 	{/if}
 </section>
